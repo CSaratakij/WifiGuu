1e59836def70fc191b4d424c8555878e74214226
Chatchai Saratakij <CSaratakij@gmail.com>
2017-09-30 19:57:23 +0700

    Make player restore his download progress in the router area.

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -3,9 +3,11 @@ extends RigidBody2D
 const MOVE_FORCE = 500
 
 onready var sprite = get_node("Sprite")
+onready var timer = get_node("Timer")
 onready var download_progress = get_node("DownloadProgress")
 
 var isFlip = false
+var isDownloading = false
 
 var moveDir = Vector2()
 var velocity = Vector2()
@@ -36,7 +38,10 @@ func _input(event):
 		moveDir.y = 0
 
 func _process(delta):
-	pass
+	if isDownloading:
+		if timer.get_time_left() == 0:
+			timer.start()
+
 
 func _fixed_process(delta):
 	if moveDir.length() > 1:
@@ -48,7 +53,21 @@ func _on_Area2D_area_enter(area):
 	var groups = area.get_groups()
 	if groups.has("router"):
 		print("Found router..")
+		isDownloading = true
+
+func _on_Area2D_area_exit( area ):
+	var groups = area.get_groups()
+	if groups.has("router"):
+		print("Exit out of router..")
+		isDownloading = false
+
+func _on_Timer_timeout():
+	if isDownloading:
+		download_progress.restore(1)
+		print("Restore : 1")
+		print("Timeout")
 
 func _flip_sprite():
 	isFlip = !isFlip
 	sprite.set_flip_h(isFlip)
+
