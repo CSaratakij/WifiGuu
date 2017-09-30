089c819c22b8b92ecfe6ee97a6328707b7b3225b
Chatchai Saratakij <CSaratakij@gmail.com>
2017-09-30 14:14:04 +0700

    Make player move according to player's input.

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -1,8 +1,11 @@
 extends RigidBody2D
 
+const MOVE_FORCE = 300
+
 onready var sprite = get_node("Sprite")
 
 var isFlip = false
+
 var moveDir = Vector2()
 var velocity = Vector2()
 
@@ -13,23 +16,21 @@ func _ready():
 	set_fixed_process(true)
 
 func _input(event):
-	if event.is_action_pressed("move_left"):
+	if Input.is_action_pressed("move_left"):
 		if not isFlip:
 			_flip_sprite()
 		moveDir.x = -1
-
-	elif event.is_action_pressed("move_right"):
+	elif Input.is_action_pressed("move_right"):
 		if isFlip:
 			_flip_sprite()
 		moveDir.x = 1
-
 	else:
 		moveDir.x = 0
 
 	if Input.is_action_pressed("move_up"):
-		moveDir.y = 1
-	elif Input.is_action_pressed("move_down"):
 		moveDir.y = -1
+	elif Input.is_action_pressed("move_down"):
+		moveDir.y = 1
 	else:
 		moveDir.y = 0
 
@@ -37,7 +38,10 @@ func _process(delta):
 	pass
 
 func _fixed_process(delta):
-	pass
+	if moveDir.length() > 1:
+		moveDir = moveDir.normalized()
+	velocity = moveDir * MOVE_FORCE
+	set_linear_velocity(velocity)
 
 func _flip_sprite():
 	isFlip = !isFlip
