8ed8bcc87a4ac911b5c3ce0b8177d9acb090d239
Chatchai Saratakij <CSaratakij@gmail.com>
2017-10-01 21:34:11 +0700

    Fixed to make game restart able.

--- src/scripts/pauseUI.gd
+++ src/scripts/pauseUI.gd
@@ -1,11 +1,12 @@
 extends Control
 
 onready var mainmenu = preload("res://scenes/mainmenu.tscn")
+onready var level = preload("res://scenes/level.tscn")
 onready var tree = get_tree()
 
 func _on_btnRestart_pressed():
 	tree.set_pause(false)
-	tree.reload_current_scene()
+	tree.change_scene_to(level)
 
 func _on_btnQuit_pressed():
 	tree.quit()
